60b8465eb962daa2d1e1000d8b66c5c2c4586d69
minirei <minrei101@gmail.com>
2022-09-03 12:34:29 +0800

    feat(entry-q2): updating cumulative notional now takes into account previous timestamp's quantity
    - implemented via `p_qty_dict` to ensure that only quantity additions that move up >0 qty_block tranches are executed
    - also added default dict for cleaner code

--- entry_challenge/q2.py
+++ entry_challenge/q2.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def to_cumulative_delayed(stream: list, quantity_block: int):
     df = []
 
@@ -12,44 +15,42 @@ def to_cumulative_delayed(stream: list, quantity_block: int):
     df.sort(key=lambda x: x[0])
 
     # put data into dicts to track their values over time
-    timestamp_dict = {}
-    cqty_dict = {}
-    cntl_dict = {}
-    # hqty_dict = {}
-    # hntl_dict = {}
+    timestamp_dict = defaultdict(lambda: [])
+    cqty_dict = defaultdict(lambda: 0)
+    cntl_dict = defaultdict(lambda: 0)
+    p_qty_dict = defaultdict(lambda: 0)
+
     for row in df:
         timestamp = row[0]
         ticker = row[1]
         qty = int(row[2])
         price = float(row[3])
-        prev_qty = 0
-        prev_price = 0
-        if(ticker in cqty_dict):
-            prev_qty = cqty_dict[ticker]
-            prev_price = cntl_dict[ticker]
-            cqty_dict[ticker] += qty
-            cntl_dict[ticker] += qty * price
-        else:
-            cqty_dict[ticker] = qty
-            cntl_dict[ticker] = qty * price
+
+        # update cumulative quantity & notional
+        cqty_dict[ticker] += qty
+        cntl_dict[ticker] += qty * price
 
         # cumulative delay adjustment
         d = cqty_dict[ticker] % quantity_block
         bqty = cqty_dict[ticker]
         bntl = cntl_dict[ticker]
+
+        # if current qty is not a multiple of quantity_block,
+        # then if adjustment adds value,
+        # then update qty and ntl
         if(d):
             bqty = cqty_dict[ticker] - d
             bntl = cntl_dict[ticker] - d * price
-            # remove duplicates
-            if(bqty == prev_qty and bntl == prev_price):
+            # if adding quantity does not move up >0 qty_block tranches
+            # then, do not add to notional
+            if(bqty == p_qty_dict[ticker]):
                 bqty = 0
-                bntl = 0
 
-        t_row = [ticker, bqty, bntl]
-        if(timestamp in timestamp_dict):
-            timestamp_dict[timestamp].append(t_row)
-        else:
-            timestamp_dict[timestamp] = [t_row]
+        # update quantity of previous ticker
+        p_qty_dict[ticker] = bqty
+
+        t_row = [ticker, bqty, round(bntl, 1)]
+        timestamp_dict[timestamp].append(t_row)
 
     # add data into output list
     output = []
@@ -57,15 +58,24 @@ def to_cumulative_delayed(stream: list, quantity_block: int):
         buffer = []
         for row in v:
             qty = row[1]
+            # only add quantities of multiples quantity_block
             if(qty % quantity_block == 0 and qty):
                 buffer.append(','.join(str(x) for x in row))
+
+        # add timestamp after to account for multiple tickers
         if buffer:
             buffer.insert(0, k)
             output.append(','.join(buffer))
 
-    print("output", output)
+    # print("output", output)
     return output
 
+
+to_cumulative_delayed([
+    '00:00,A,6,5.6',
+    '00:01,A,5,5.6',
+    '00:02,A,1,5.6',
+], 5)
 
 to_cumulative_delayed([
     '00:05,A,1,5.6',
@@ -77,8 +87,8 @@ to_cumulative_delayed([
 ], 5)
 
 to_cumulative_delayed([
-    '00:01,A,5,5.5',
-    '00:00,A,4,5.6',
+    # '00:01,A,5,5.5',
+    # '00:00,A,4,5.6',
     '00:00,B,5,5.5',
     '00:02,B,4,5.6',
 ], 5)
