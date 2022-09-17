269075535a3825a7553fe7e7c8a06b196489db71
ActualNugget <isvaran@tuta.io>
2022-09-17 23:17:48 +0800

    warmup stig real

--- myapp/routes/warmup.py
+++ myapp/routes/warmup.py
@@ -12,24 +12,29 @@ logger = logging.getLogger(__name__)
 @app.route('/stig/warmup', methods=['POST'])
 def warmup():
     data = request.get_json()
-    logging.info(data)
-    questions = data[0].get("questions")
-    lower = int(questions[0][0])
-    higher = int(questions[0][1])
-    max = int(data[0].get("maxRating"))
-    right = 0
-    for i in range(1,max+1):
-        if lower <= i and i <= higher:
-            guess = lower
-        elif lower > 1:
-            guess = 1
-        else:
-            guess = higher + 1
-        if guess == i:
-            right += 1
-
-    gcd = np.gcd(right, max)
+    interview = data[0]
+    max = interview["maxRating"]
+    lower = np.array()
+    upper = np.array()
+    for pair in interview["questions"]:
+        lower = np.append(lower, pair["lower"])
+        upper = np.append(upper, pair["upper"])
+    guessable = 0
+    for rating in range(1,max+1):
+        floor = max
+        unasked = True
+        for pair in range(lower.size):
+            if lower[pair] <= rating and rating <= upper[pair] and lower[pair] < floor:
+                floor = lower[pair]
+                unasked = False
+        if rating == floor:
+            guessable += 1
+        if unasked == True:
+            add_one = True
+    if add_one == True:
+        guessable += 1
+    gcd = np.gcd(guessable, max)
     result = {}
-    result["p"] = int(right / gcd)
+    result["p"] = int(guessable / gcd)
     result["q"] = int(max / gcd)
-    return json.dumps(result)
+    return [json.dumps(result)]
