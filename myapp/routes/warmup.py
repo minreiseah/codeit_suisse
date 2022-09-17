a2ad50167d742208d0c310586029274d70a6bfc1
ActualNugget <isvaran@tuta.io>
2022-09-17 23:39:02 +0800

    loopy

--- myapp/routes/warmup.py
+++ myapp/routes/warmup.py
@@ -13,25 +13,27 @@ logger = logging.getLogger(__name__)
 def warmup():
     data = request.get_json()
     # logging.info(data)
-    interview = data[0]
-    max = interview["maxRating"]
-    guessable = 0
-    add_one = False
-    for rating in range(1,max+1):
-        floor = max
-        unasked = True
-        for pair in interview["questions"]:
-            if pair["lower"] <= rating and rating <= pair["upper"] and pair["lower"] < floor:
-                floor = pair["lower"]
-                unasked = False
-        if rating == floor:
+    output = []
+    for interview in data:
+        max = interview["maxRating"]
+        guessable = 0
+        add_one = False
+        for rating in range(1,max+1):
+            floor = max
+            unasked = True
+            for pair in interview["questions"]:
+                if pair["lower"] <= rating and rating <= pair["upper"] and pair["lower"] < floor:
+                    floor = pair["lower"]
+                    unasked = False
+            if rating == floor:
+                guessable += 1
+            if unasked == True:
+                add_one = True
+        if add_one == True:
             guessable += 1
-        if unasked == True:
-            add_one = True
-    if add_one == True:
-        guessable += 1
-    gcd = np.gcd(guessable, max)
-    result = {}
-    result["p"] = int(guessable / gcd)
-    result["q"] = int(max / gcd)
-    return json.dumps(result)
+        gcd = np.gcd(guessable, max)
+        result = {}
+        result["p"] = int(guessable / gcd)
+        result["q"] = int(max / gcd)
+        output.append(result)
+    return json.dumps(output)
