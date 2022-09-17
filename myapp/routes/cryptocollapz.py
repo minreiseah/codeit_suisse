b0afea2ef0d9b16a4e477441c9bdb1ff7c754ad2
minirei <minrei101@gmail.com>
2022-09-17 16:17:35 +0800

    collatz

--- myapp/routes/cryptocollapz.py
+++ myapp/routes/cryptocollapz.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import math
 
 from flask import request, jsonify
 from myapp import app
@@ -11,23 +12,16 @@ def cryptocollapz():
     data = request.get_json()
     result = []
     memo = {}
-    for tc in data:
-        tc_out = []
-        for i in tc:
-            start = i
-            max = i
-            while i != 1:
-                if i % 2 != 0:
-                    i = i * 3 + 1
-                    if i in memo.keys():
-                        max = memo[i]
-                        break
-                    if i > max:
-                        max = i
-                i /= 2
-            memo[start] = max
-            tc_out.append(max)
-        result.append(tc_out)
+    for xs in data:
+        buf = []
+        for x in xs:
+            if x in memo:
+                buf.append(memo[x])
+            else:
+                y = int(calc_max(x))
+                memo[x] = y
+                buf.append(y)
+        result.append(buf)
     return jsonify(result)
 
 @app.route('/cryptocollapztest', methods=['GET'])
@@ -54,20 +48,40 @@ def cryptocollapztest():
         result.append(tc_out)
     return jsonify(result)
 
-def calc_max(x : int):
-    mx = max(x, 4)
-    buf = [x]
-    count = 0
-    while(True):
-        if(x == 1 or count == 100):
-            break
-        if(x % 2 == 0):
-            x /= 2
-        else:
-            x = x * 3 + 1
-        mx = max(mx, x)
-        buf.append(x)
-        count += 1
-    return int(mx)
-         
 
+def calc_max(x : int):
+    n = math.log2(x)
+    if(n == int(n)):
+        return x
+
+    if(x % 2 == 0): #even
+        x /= 2
+        return max(x, calc_max(x))
+    else:
+        x = 3 * x + 1
+        return max(x, calc_max(x))
+
+# def calc_max(x : int):
+#     mx = max(x, 4)
+#     buf = [x]
+#     n = math.log2(x)
+#     if(n == int(n)):
+#         return x
+#
+#     r = x - int(n) # x = 2^n + r
+#     return r
+#
+#
+#     while(True):
+#         if(x == 1):
+#             break
+#         if(x % 2 == 0):
+#             x /= 2
+#         else:
+#             x = x * 3 + 1
+#         mx = max(mx, x)
+#         buf.append(x)
+#         count += 1
+#     return int(mx)
+#          
+#
--- myapp/routes/test.py
+++ myapp/routes/test.py
@@ -1,5 +1,28 @@
 import datetime as dt
+import math
 
-a = dt.date(2001,1,10)
-a += dt.timedelta(1)
-print(a)
+memo = {}
+
+def calc_max(x : int):
+    n = math.log2(x)
+    if(n == int(n)):
+        return x
+
+    if(x % 2 == 0): #even
+        x /= 2
+        return max(x, calc_max(x))
+    else:
+        x = 3 * x + 1
+        return max(x, calc_max(x))
+
+
+my_list = [1,2,3,4,5,6,7,8,9,10]
+
+for x in my_list:
+    if x in memo:
+        print(memo[x])
+    else:
+        y = int(calc_max(x))
+        memo[x] = y
+        print(y)
+    
